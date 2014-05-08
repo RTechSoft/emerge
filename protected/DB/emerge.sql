534ca9ae6e46ef51b39e80b17279ba3746fd8548
rienier <rienierpatron@gmail.com>
2014-05-09 00:01:58 +0800

    DB added

---
+++ protected/DB/emerge.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 08, 2014 at 05:59 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `emerge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agencies`
+--
+
+CREATE TABLE IF NOT EXISTS `agencies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `agency_username` varchar(100) NOT NULL,
+  `agency_password` text NOT NULL,
+  `agency_name` varchar(100) NOT NULL,
+  `agency_location` text NOT NULL,
+  `agency_type` int(11) NOT NULL,
+  `location_scope` varchar(100) NOT NULL,
+  `agency_verified` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agency_types`
+--
+
+CREATE TABLE IF NOT EXISTS `agency_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `help_logs`
+--
+
+CREATE TABLE IF NOT EXISTS `help_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_id` int(11) NOT NULL,
+  `agency_id` int(11) NOT NULL,
+  `response_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `help_requests`
+--
+
+CREATE TABLE IF NOT EXISTS `help_requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender_number` int(11) NOT NULL,
+  `sender_location` text NOT NULL,
+  `respondents` text NOT NULL,
+  `location_scope` text NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_firstname` varchar(100) NOT NULL,
+  `user_middlename` varchar(100) NOT NULL,
+  `user_lastname` varchar(100) NOT NULL,
+  `user_mobile` int(11) NOT NULL,
+  `user_password` text NOT NULL,
+  `user_address` text NOT NULL,
+  `user_mobile_verification_code` int(11) NOT NULL,
+  `user_status` int(11) NOT NULL,
+  `registration_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
