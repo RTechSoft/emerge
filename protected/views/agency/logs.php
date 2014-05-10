32305ad344b691ed17e9fe6c50d0ff4df2fc46c3
junerey <junerey@junerey.(none)>
2014-05-10 15:30:59 +0800

    temp help logs

---
+++ protected/views/agency/logs.php
@@ -0,0 +1,29 @@
+<div class="row">
+	<div class="col-sm-12">
+		<div class="page-header">
+			<div class="clearfix">
+				<h3 class="content-title pull-left">Logs</h3>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Date</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>1</td>
+					<td>1</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
